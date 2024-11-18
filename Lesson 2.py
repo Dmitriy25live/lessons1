ca01c96e4bcd28b6f5b6f0fb5c0fc025f58257ef
Dmitriy25live <mafia.dm1980@gmail.com>
2024-11-18 13:17:51 +0300

    Add files via upload

---
+++ Lesson 2.py
@@ -0,0 +1,17 @@
+print('Hello, World')
+print(12+89-1)
+# 1st program
+print(9**0.5*5)
+#2nd program
+print(9.99>9.98,1000!=1000.1)
+# 3rd program
+print(2*2+2, 2*(2+2))
+print(2*2+2==2*(2+2))
+# 4th program
+print('123.456')
+print(type('123.456'))
+print(4)
+print('123.456' , 123.456)
+print(123.456*10)
+print(int(123.456))
+print(float(123.456)*10)
